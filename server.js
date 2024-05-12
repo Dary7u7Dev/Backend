1647607aad4156719787d5a5269e9682d0ff0097
Dary7u7Dev <yibania65@gmail.com>
2024-05-12 02:51:47 -0700

    Inicializar repositorio con mi proyecto

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const app = express();
+const port = 8080;
+const prisma = new PrismaClient();
+
+
+
+app.get('/', (req, res) => {
+  res.send("Servidor Backend Global Stuxiom Host localhost");
+});
+
+// Aqui van mis llamadas a la base de datos, recuerda modificar esto ya que ira para un servidor global
+app.get('/planes', async (req, res) => {
+  try {
+    const planes = await prisma.planes.findMany();
+    res.json(planes);
+  } catch (error) {
+    console.error("Error al obtener los planes:", error);
+    res.status(500).json({ error: 'Error al obtener los planes' });
+  }
+});
+//----------------------------------------------------------
+
+app.listen(port, () => {
+  console.log(`ip:${port}`);
+});
